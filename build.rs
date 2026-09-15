3fc8398dc2408c51e2998a1d53739c8267363cf2
moadmoaz32-dev <moadmoaz32@gmail.com>
2026-09-15 12:34:51 +0700

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,12 +1,12 @@
 fn main() {
-    // Chỉ biên dịch lại C++ nếu file này bị sửa đổi
     println!("cargo:rerun-if-changed=c_src/core_simd.cpp");
 
     cc::Build::new()
         .cpp(true)
         .file("c_src/core_simd.cpp")
-        .flag("-mavx2")     // Kích hoạt thanh ghi 256-bit
-        .flag("-O3")        // Tối ưu hóa cực hạn
-        .flag("-std=c++17") // Dùng chuẩn C++17
+        .flag("-O3")
+        .flag("-mavx2")
+        .flag("-mbmi2") // Kích hoạt lệnh nhân số cực lớn của Intel
+        .flag("-funroll-loops") // Ép bung vòng lặp
         .compile("core_simd");
 }
